bfceaf02ccf8fc0f3916090958e6d6b33681957f
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-02-07 17:33:44 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import AppTitle from "./components/AppTitle.vue";
+export default {
+  components: {
+    AppTitle,
+  },
+};
+</script>
+
+<template>
+  <AppTitle></AppTitle>
+</template>
+
+<style>
+@import url(./assets/style.css);
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Primo titolo con Vue + Vite",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 4rem;
+  color: cornflowerblue;
+}
+</style>
